d9676560003fadc3c74a71bee89cecdacd2ab3d7
Chris Schick <seeschickrun@gmail.com>
2022-02-28 14:32:05 -0600

    Re-implemented the display data struct
    I had initially tried to make the display label part of the data struct.
    This became confusing when trying to access the label in the builder
    file through the template binds. Now the data is it's own struct and
    leaves managing the label to the window, AS IT ALWAYS SHOULD HAVE BEEN.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# nums
+# Nums
+
 A calculator written in C using GTK4.
+
+This project is my first attempt to apply the techniques expressed in the [GTK tutorials](https://docs.gtk.org/gtk4/getting_started.html).
+
+This is meant to be a learning experience, not an amazing calculator! If you have come to evaluate the code please feel free to suggest improvements that would help me learn to use GTK better.
--- src/nums_app_win.c
+++ src/nums_app_win.c
@@ -23,74 +23,57 @@
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
 
-enum { NUMS_DISPLAY_MAXBUFF = 16 };
+enum { NUMS_DATA_MAXBUFF = 16 };
 
-struct nums_display {
-	GtkLabel* screen;
-	char buffer[NUMS_DISPLAY_MAXBUFF];
+struct nums_data {
+	char buffer[NUMS_DATA_MAXBUFF];
 };
 
 static void
-nums_display_update(struct nums_display* display)
+nums_data_init(struct nums_data* data)
 {
-	const char* format = "<span size=\"x-large\">%s</span>";
-	char* markup = g_markup_printf_escaped(format, display->buffer);
-
-	gtk_label_set_markup(GTK_LABEL(display->screen), markup);
-
-	g_free(markup);
+	strcpy(data->buffer, "0");
 }
 
 static void
-nums_display_clear(struct nums_display* display)
+nums_data_clear(struct nums_data* data)
 {
-	strcpy(display->buffer, "0");
+	strcpy(data->buffer, "0");
 }
 
 static void
-nums_display_init(struct nums_display* display)
+nums_data_append_number(struct nums_data* data, const char* digit)
 {
-	gtk_widget_set_halign(GTK_WIDGET(display->screen), GTK_ALIGN_END);
-	nums_display_clear(display);
-	nums_display_update(display);
-}
-
-static void
-nums_display_append_number(struct nums_display* display, const char* digit)
-{
-	if (strcmp(display->buffer, "0") == 0) {
-		strcpy(display->buffer, digit);
-	} else if (strlen(display->buffer) < NUMS_DISPLAY_MAXBUFF - 1) {
-		strcat(display->buffer, digit);
+	if (strcmp(data->buffer, "0") == 0) {
+		strcpy(data->buffer, digit);
+	} else if (strlen(data->buffer) < NUMS_DATA_MAXBUFF - 1) {
+		strcat(data->buffer, digit);
 	}
-	nums_display_update(display);
 }
 
 static void
-nums_display_append_decimal(struct nums_display* display)
+nums_data_append_decimal(struct nums_data* data)
 {
-	char* s = display->buffer;
+	char* s = data->buffer;
 	int len = strlen(s);
-	if (!strchr(s, '.') && len < NUMS_DISPLAY_MAXBUFF - 1) {
+	if (!strchr(s, '.') && len < NUMS_DATA_MAXBUFF - 1) {
 		s[len++] = '.';
 		s[len] = '\0';
-		nums_display_update(display);
 	}
 }
 
 static void
-nums_display_change_sign(struct nums_display* display)
+nums_data_change_sign(struct nums_data* data)
 {
-	char* s = display->buffer;
+	char* s = data->buffer;
 	if (s[0] == '-') {
 		for (char* p = s+1; *s; ++s, ++p)
 			*s = *p;
-	} else if (strlen(s) < NUMS_DISPLAY_MAXBUFF - 1) {
+	} else if (strlen(s) < NUMS_DATA_MAXBUFF - 1) {
 		for (char a = '-', b = *s; a; *s = a, a = b, ++s)
 			b = b ? *s : b;	// change b until it is '\0'
 		*s = '\0';
 	}
-	nums_display_update(display);
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
@@ -98,61 +81,101 @@ nums_display_change_sign(struct nums_display* display)
 struct _NumsAppWindow {
 	GtkApplicationWindow parent;
 
-	struct nums_display display;
+	GtkLabel* display;
+
+	struct nums_data* state;
 };
 
 G_DEFINE_TYPE(NumsAppWindow, nums_app_window, GTK_TYPE_APPLICATION_WINDOW);
 
 static void
+update_display(GtkLabel* display, struct nums_data* state)
+{
+	const char* format = "<span size=\"x-large\">%s</span>";
+	char* markup = g_markup_printf_escaped(format, state->buffer);
+
+	gtk_label_set_markup(GTK_LABEL(display), markup);
+
+	g_free(markup);
+}
+
+static void
+clear_cb(GtkWidget* w, gpointer p)
+{
+	NumsAppWindow* window = NUMS_APP_WINDOW(p);
+
+	nums_data_clear(window->state);
+	update_display(window->display, window->state);
+}
+
+static void
+number_cb(GtkWidget* w, gpointer p)
+{
+	NumsAppWindow* window = NUMS_APP_WINDOW(p);
+
+	const char* digit = gtk_button_get_label(GTK_BUTTON(w));
+	nums_data_append_number(window->state, digit);
+	update_display(window->display, window->state);
+}
+
+static void
+decimal_cb(GtkWidget* w, gpointer p)
+{
+	NumsAppWindow* window = NUMS_APP_WINDOW(p);
+
+	nums_data_append_decimal(window->state);
+	update_display(window->display, window->state);
+}
+
+static void
+change_sign_cb(GtkWidget* w, gpointer p)
+{
+	NumsAppWindow* window = NUMS_APP_WINDOW(p);
+	nums_data_change_sign(window->state);
+	update_display(window->display, window->state);
+}
+
+static void
 nums_app_window_init(NumsAppWindow* window)
 {
 	gtk_widget_init_template(GTK_WIDGET(window));
-	nums_display_init(&window->display);
+
+	window->state = malloc(sizeof(struct nums_data));
+	nums_data_init(window->state);
+
+	gtk_widget_set_halign(GTK_WIDGET(window->display), GTK_ALIGN_END);
+	update_display(window->display, window->state);
 }
 
 static void
-number_pressed(GtkWidget* w, gpointer p)
+nums_app_window_dispose(GObject* obj)
 {
-	GtkButton* btn = GTK_BUTTON(w);
-	NumsAppWindow* window = NUMS_APP_WINDOW(p);
+	NumsAppWindow* window = NUMS_APP_WINDOW(obj);
 
-	const char* digit = gtk_button_get_label(btn);
-	nums_display_append_number(&window->display, digit);
+	free(window->state);
+
+	G_OBJECT_CLASS(nums_app_window_parent_class)->dispose(obj);
 }
 
 static void
-decimal_pressed(GtkWidget* w, gpointer p)
+nums_app_window_class_init(NumsAppWindowClass* klass)
 {
-	NumsAppWindow* window = NUMS_APP_WINDOW(p);
-	nums_display_append_decimal(&window->display);
-	/*
-	struct nums_display* q = (struct nums_display*)p;
-	nums_display_append_decimal(q);
-	*/
-}
+	G_OBJECT_CLASS(klass)->dispose = nums_app_window_dispose;
 
-static void
-change_sign_pressed(GtkWidget* w, gpointer p)
-{
-	NumsAppWindow* window = NUMS_APP_WINDOW(p);
-	nums_display_change_sign(&window->display);
-}
-
-static void
-nums_app_window_class_init(NumsAppWindowClass* class)
-{
-	gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class),
+	gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(klass),
 			"/com/github/Chrinkus/nums/window.ui");
 
-	gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class),
+	gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(klass),
 			NumsAppWindow, display);
 
-	gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class),
-			number_pressed);
-	gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class),
-			decimal_pressed);
-	gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class),
-			change_sign_pressed);
+	gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(klass),
+			clear_cb);
+	gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(klass),
+			number_cb);
+	gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(klass),
+			decimal_cb);
+	gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(klass),
+			change_sign_cb);
 }
 
 NumsAppWindow*

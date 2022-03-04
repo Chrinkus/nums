fd1a723f5637cbbf2da653c77b540537e3a19802
Chris Schick <seeschickrun@gmail.com>
2022-03-04 10:44:29 -0600

    Out-sourced string utils to cgs library
    - I really wanted to use tested utilities
    - cgs is not yet installed as a package so its inclusion is as a regular
      library

--- src/nums_data.c
+++ src/nums_data.c
@@ -22,52 +22,7 @@
 #include <string.h>
 #include <stdio.h>	/* snprintf */
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
-
-/**
- * str_append
- *
- * Add a character to the end of a string. Caller is responsible for
- * ensuring there is enough room.
- */
-static void
-str_append(char* s, char ch)
-{
-	while (*s)
-		++s;
-	*s++ = ch;
-	*s = '\0';
-}
-
-/**
- * str_prepend
- *
- * Add a character to the front of a string. Caller is responsible for
- * ensuring there is enough room.
- */
-static void
-str_prepend(char* s, char ch)
-{
-	for (char tmp = *s; ch; *s++ = ch, ch = tmp)
-		if (tmp)
-			tmp = *s;
-	*s = '\0';
-}
-
-/**
- * str_shift
- *
- * Shift all characters towards the front of the string, removing first
- * character.
- */
-static void
-str_shift(char* s)
-{
-	for (char* p = s+1; *s; ++s, ++p)
-		*s = *p;
-}
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+#include <cgs/cgs.h>	/* string utils */
 
 enum nums_data_buffer_size { NUMS_DATA_MAXBUFF = 16 };
 
@@ -137,6 +92,7 @@ nums_data_eval(struct nums_data* data)
 
 	if (data->op != NUMS_OP_ERROR) {
 		snprintf(data->buffer, NUMS_DATA_MAXBUFF, "%f", data->store);
+		cgs_strtrimch(data->buffer, '0');
 		data->op = NUMS_OP_NONE;
 		data->store = 0.0;
 	}
@@ -159,7 +115,7 @@ nums_data_append_digit(struct nums_data* data, char digit)
 	if (strcmp(data->buffer, "0") == 0)
 		data->buffer[0] = digit;
 	else if (strlen(data->buffer) < NUMS_DATA_MAXBUFF - 1)
-		str_append(data->buffer, digit);
+		cgs_strappend(data->buffer, digit);
 }
 
 void
@@ -167,15 +123,15 @@ nums_data_append_decimal(struct nums_data* data)
 {
 	if (!strchr(data->buffer, '.') &&
 			strlen(data->buffer) < NUMS_DATA_MAXBUFF - 1)
-		str_append(data->buffer, '.');
+		cgs_strappend(data->buffer, '.');
 }
 
 void
 nums_data_change_sign(struct nums_data* data)
 {
 	if (data->buffer[0] == '-')
-		str_shift(data->buffer);
+		cgs_strshift(data->buffer, 1);
 	else if (strlen(data->buffer) < NUMS_DATA_MAXBUFF - 1)
-		str_prepend(data->buffer, '-');
+		cgs_strprepend(data->buffer, '-');
 }
 
